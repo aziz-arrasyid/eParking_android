f739204f71f141545137effda7f8501537af1b81
KelvinNep <nivlzk050306@gmail.com>
2023-10-10 09:39:41 +0700

    sad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ android {
 
 dependencies {
 
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
